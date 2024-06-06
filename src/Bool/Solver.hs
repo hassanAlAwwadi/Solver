a393381fbff9f9f772c9cb9ef820089e9134c5ba
hassanAlAwwadi <hassan.awwadi@gmail.com>
2024-06-06 12:49:16 +0200

    extra solver algorithm

--- src/Bool/Solver.hs
+++ src/Bool/Solver.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE LambdaCase #-}
 module Bool.Solver where 
-import Bool.Raw 
 import Bool.CNF 
-import Bool.Parser 
-import Bool.Translation
-import Data.Map as M
+import qualified Data.Map as M
+import qualified Data.List as L
+import Data.Map(Map)
 import Control.Applicative(asum)
 
-sat_tree_method :: CNF -> Maybe (Map String Bool) 
-sat_tree_method (CNF clauses) = go clauses M.empty where 
+sat_brute_force :: CNF -> Maybe (Map String Bool) 
+sat_brute_force (CNF clauses) = go clauses M.empty where 
   go :: [Clause] -> Map String Bool -> Maybe (Map String Bool)
   go [] m                      = Just m 
   go ((Clause lits):cs) litmap = let 
@@ -24,3 +23,23 @@ sat_tree_method (CNF clauses) = go clauses M.empty where
     Just status -> if status == desire 
       then m : go' lits m 
       else go' lits m -- kill this branch
+
+sat_filter :: CNF -> Maybe (Map String Bool)
+sat_filter (CNF clauses) = go (L.sortOn length $ map (\(Clause c) -> c) clauses) M.empty where
+  go :: [[Lit]] -> Map String Bool -> Maybe (Map String Bool)
+  go [] acc = Just acc
+  go (l:ls) acc = asum $ flip fmap l $ \lit -> let 
+  -- | variable name, desired value, and opposite form
+    (n, d, o) = case lit of Lit v -> (v, True, Neg v); Neg v -> (v, False, Lit v)
+    acc'    = M.insert n d acc  
+  -- | If we have set a to True, we can filter all clauses with a in them,
+  -- | and if we've set it to false, we can filter all clauses with !a in them.
+    ls'  = filter (notElem lit) ls      
+  -- | If we've set a to True, we can remove !a from all clauses, since we can't set it to false anymore (and vice versa). 
+  -- | This works out because if we remove all literals from a clause, asum [] will return Nothing :)
+    ls'' = map    (filter (/= o)) ls'
+  -- | We reorder based on length of the lists, to narrow down branching algorithms.
+  -- | Since we know the maxsize is 3, and the minsize is 0, there is probably a better bounded sorting algorithm
+    ls''' = L.sortOn length ls''
+    in go ls''' acc'                     
+    
